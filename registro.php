526cae37cf03537468581217e7773a5a061cecf9
Hazael Lopez <elhazael134@gmail.com>
2020-04-02 23:01:35 -0600

    Incluimos las cabecera, barra de navegacion y el cierre en registro.php

--- registro.php
+++ registro.php
@@ -1,1 +1,12 @@
+<?php
+    
+    $titulo = "Registro";
+    
+    include_once 'plantillas/documento-inicio.inc.php';
+    include_once 'plantillas/navbar.inc.php';
+?>
 
+
+<?php
+    include_once 'plantillas/documento-cierre.inc.php';
+?>
