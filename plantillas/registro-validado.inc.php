c05ffba7b605e4cf74784a073bcc057f2217b895
Walter Díaz <lwalter_19@hotmail.com>
2020-04-03 17:21:37 -0600

    Agregamos los campos para el Registro para Validar el ingreso en el archivo registro-validado.inc.php

--- plantillas/registro-validado.inc.php
+++ plantillas/registro-validado.inc.php
@@ -1,1 +1,21 @@
-
+<div class="form-group">   
+    <label>Nombre De Usuario</label>
+    <input type="text" class="form-control" placeholder="Nombre-Apellidos!!!" name="nombre">
+</div>
+<div class="form-group">   
+    <label>Email</label>
+    <input type="text" class="form-control" placeholder="NombreApellidos@mail.com!!!" name="email">
+</div>
+<div class="form-group">   
+    <label>Contraseña</label>
+    <input type="password" class="form-control" placeholder="" name="clave1">
+</div>
+<div class="form-group">   
+    <label>Repite la Contraseña</label>
+    <input type="password" class="form-control" placeholder="" name="clave2">
+</div>
+<br><br>
+<button type="reset" class="btn btn-default btn-primary" name="limpiar">Limpiar Formulario</button>
+<br><br>
+<button type="submit" class="btn btn-default btn-primary" name="enviar">Enviar Formulario</button>
+<br><br>
