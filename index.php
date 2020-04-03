aa19beb45a1da9fd6879878d682e9486d23bd580
Hazael Lopez <elhazael134@gmail.com>
2020-04-02 23:01:17 -0600

    Incluimos las cabecera, barra de navegacion y el cierre en index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
+<?php
+    
+    $titulo = "blog De JavaDevOne";
+    
+    include_once 'plantillas/documento-inicio.inc.php';
+    include_once 'plantillas/navbar.inc.php';
+?>
 
+
+<?php
+    include_once 'plantillas/documento-cierre.inc.php';
+?>
