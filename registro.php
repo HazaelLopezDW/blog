db73d13c570339d701e2e3e4b2cb8e71d713b187
Walter Díaz <lwalter_19@hotmail.com>
2020-04-03 17:12:17 -0600

    Agregamos una fila con dos columnas, en una damos la instruciones y la otra es para un formulario de registro al sitio web

--- registro.php
+++ registro.php
@@ -6,7 +6,48 @@
     include_once 'plantillas/documento-inicio.inc.php';
     include_once 'plantillas/navbar.inc.php';
 ?>
+<div class="container">
+    <div class="jumbotron">
+        <h1>Formulario De Registro</h1>
+    </div>
+</div>
 
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 text-center">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">
+                                Instrucciones
+                            </h3>
+                        </div>
+                        <div class="panel-body">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 text-center">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">
+                                Ingresa tus datos
+                            </h3>
+                        </div>
+                        <div class="panel-body">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php
     include_once 'plantillas/documento-cierre.inc.php';
