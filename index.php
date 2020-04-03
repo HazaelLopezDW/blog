15da39ca38e4579042175732f6ce411836dc6794
Walter Díaz <lwalter_19@hotmail.com>
2020-04-03 17:11:06 -0600

    Agregamos filas y columnas a nuestro documento index.php

--- index.php
+++ index.php
@@ -14,6 +14,66 @@
     </div>
 </div>
 
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <span class="glyphicon glyphicon-search" data-hidden="true"></span> Buscar
+                        </div>
+                        <div class="panel-body">
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="" placeholder="Pudes hacer mas de 100,000 busqueda!!!"> 
+                            </div>
+                            <button type="button" class="form-control" name="">Buscar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <span class="glyphicon glyphicon-filter" data-hidden="true"></span> Filtros
+                        </div>
+                        <div class="panel-body">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <span class="glyphicon glyphicon-calendar" data-hidden="true"></span> Archivos
+                        </div>
+                        <div class="panel-body">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <span class="glyphicon glyphicon-time" data-hidden="true"></span> Ultimas Entradas
+                        </div>
+                        <div class="panel-body">
+                            <p>Aun no hay entradas que mostrar</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php
     include_once 'plantillas/documento-cierre.inc.php';
 ?>
