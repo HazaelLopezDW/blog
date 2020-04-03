96d845cfb6811eaaadc4a9e7a5f27f4e24068507
Hazael Lopez <elhazael134@gmail.com>
2020-04-02 22:39:00 -0600

    Creamos la cabezera para nuestro sitio Web en el archivo plantillas/documento-inicio.inc.php

--- plantillas/documento-inicio.inc.php
+++ plantillas/documento-inicio.inc.php
@@ -1,1 +1,18 @@
-
+<!DOCTYPE html>
+<html> 
+    <head>
+        <?php 
+            if(!isset($titulo) && empty($titulo)){
+                $titulo = "blog de JavaDevOne";
+            }
+            echo "<title>$titulo</title>";
+        ?>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="MetaData para los motores de busqueda">
+        
+        <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="../css/estilos.css">
+    </head>
+    <body>
