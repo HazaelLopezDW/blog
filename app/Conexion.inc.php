84bed0ec1a9147a750a8f3fb01b8b7b72c0d36f7
Hazael Lopez <elhazael134@gmail.com>
2020-04-03 18:08:27 -0600

    Agragamos el archivo app/Conexion.inc.php y creamor tres funciones para abrir, cerrar y obtener la conexion

---
+++ app/Conexion.inc.php
@@ -0,0 +1,36 @@
+<?php 
+    class Conexion{
+        
+        private static $conexion;
+        
+        public static function abrir_conexion(){
+            if(!isset(self::$conexion)){
+                try{
+                    include_once 'Config.inc.php';
+                    
+                    self::$conexion = new PDO("mysql:host=".NOMBRE_SERVIDOR."; dbname=".NOMBRE_BD, NOMBRE_USUARIO, PASSWORD);
+                    self::$conexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    self::$conexion -> exec("SET CHARACTER SET utf8");
+                    
+                    print "Conexion abierta <br>";
+                } catch (PDOException $ex) {
+                    print "ERROR:" . $ex -> getMessage() . "<br>";
+                }
+            }
+        }
+        
+        public static function cerrar_conexion(){
+            if(isset(self::$conexion)){
+                try{
+                    self::$conexion = null;
+                    die();
+                } catch (PDOException $ex) {
+                    print "ERROR:" . $ex -> getMessage() . "<br>";
+                }
+            }
+        }
+        
+        public static function obtener_conexion(){
+            return self::$conexion;
+        }
+    }
