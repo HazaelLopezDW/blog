39a2a018f25818bb61f32309f10278ce3a97214a
Walter Díaz <lwalter_19@hotmail.com>
2020-04-04 09:25:11 -0600

    Creamos la funcion obtener_todos(), para obtener el total de usuarios

--- app/RepositorioUsuario.inc.php
+++ app/RepositorioUsuario.inc.php
@@ -1,1 +1,30 @@
-
+<?php
+    class RepositorioUsuario{
+        
+        public static function obtener_todos($conexion){
+            $total_usuarios = array();
+            
+            if(isset($conexion)){
+                try{
+                    include_once 'Usuario.inc.php';
+                    
+                    $sql = "SELECT * FROM usuarios";
+                    $sentencia = $conexion -> prepare($sql);
+                    $sentencia -> execute();
+                    
+                    $resultado = $sentencia -> fetchAll();
+                    
+                    if(count($resultado)){
+                        foreach ($resultado as $filas){
+                            $total_usuarios[] = new Usuario($fila['id'], $fila['nombre'], $fila['email'], $fila['password'], $fila['fecha_registro'], 
+                                    $fila['activo']);
+                        }
+                    }else{
+                        print "No hay Usuarios que mostrar <br>";
+                    }
+                } catch (PDOException $ex) {
+                    print "ERROR:" . $ex -> getMessage() . "<br>";
+                }
+            }
+        }
+    }
