2ebffdc21a0c4a64b3f01b6f6c47973fc850221a
Walter Díaz <lwalter_19@hotmail.com>
2020-04-05 11:23:44 -0600

    Creamos el archivo registro-correcto y pusimos la cabecera del sitio y el cierre de el sitio

---
+++ registro-correcto.php
@@ -0,0 +1,18 @@
+<?php
+    include_once 'app/Config.inc.php';
+    include_once 'app/Conexion.inc.php';
+    include_once 'app/RepositorioUsuario.inc.php';
+    include_once 'app/ValidadorRegistro.inc.php';
+    include_once 'app/Usuario.inc.php';
+    
+    $titulo = "Registro-Correcto";
+    
+    include_once 'plantillas/documento-inicio.inc.php';
+    include_once 'plantillas/navbar.inc.php';
+?>
+
+
+
+<?php
+    include_once 'plantillas/documento-cierre.inc.php';
+?>
