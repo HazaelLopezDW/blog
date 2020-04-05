7af4e36ffdb7f17b575343d80d8abb22b714b36a
Walter Díaz <lwalter_19@hotmail.com>
2020-04-05 10:45:38 -0600

    Creamos la funcion insertar_usuario(,) con el cual insertamos el usuario en la DB

--- app/RepositorioUsuario.inc.php
+++ app/RepositorioUsuario.inc.php
@@ -86,6 +86,30 @@
             }
             return $email_existe;
         }
+        
+        public function insertar_usuario($conexion, $usuario){
+            $insertar_usuario = null;
+            
+            $usrNom = $usuario -> obtener_nombre();
+            $usrEmail = $usuario -> obtener_email();
+            $usrpassword = $usuario -> obtener_password();
+            
+            if(isset($conexion)){
+                try{
+                    
+                    $sql = "INSERT INTO usuarios(nombre, email, password, fecha_registro, activo) VALUES(:nombre, :email, :password, NOW(), 0)";
+                    $sentencia = $conexion -> prepare($sql);
+                    $sentencia -> bindParam(":nombre", $usrNom, PDO::PARAM_STR);
+                    $sentencia -> bindParam(":email", $usrEmail, PDO::PARAM_STR);
+                    $sentencia -> bindParam(":password", $usrpassword, PDO::PARAM_STR);
+                    
+                    $insertar_usuario = $sentencia -> execute();
+                } catch (PDOException $ex) {
+                    print "ERROR: Walter Hazael" . $ex -> getMessage() . "<br>";
+                }
+            }
+            return $insertar_usuario;
+        }
     }
 
     
