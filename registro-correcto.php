f5ab7002005f5f752c3342f050cfffcebe606e91
Hazael Lopez <elhazael134@gmail.com>
2020-04-05 11:53:16 -0600

    Craamos el panel para dar la bienvenida al usuario y dejamos el enlace para que pueda hacer su login para usuar su cuenta

--- registro-correcto.php
+++ registro-correcto.php
@@ -5,12 +5,35 @@
     include_once 'app/ValidadorRegistro.inc.php';
     include_once 'app/Usuario.inc.php';
     
-    $titulo = "Registro-Correcto";
+    if(isset($_GET['nombre'])){
+        $nombre = $_GET['nombre'];
+    }
+    
+    $titulo = "!Registro-Correcto";
     
     include_once 'plantillas/documento-inicio.inc.php';
     include_once 'plantillas/navbar.inc.php';
 ?>
 
+<div class="container">
+    <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <span class="glyphicon glyphicon-ok-circle" data-hidden="true"></span> Registro Correcto
+                        </div>
+                        <div class="panel-body">
+                            <P class="text-center">!Gracias por registrate <b><?php echo $nombre; ?>!</b>
+                            <br><br>
+                            <a href="<?php echo RUTA_LOGIN;?>">Inicia Sesión</a> Para hacer uso de tu cuenta
+                            </P>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
 
 
 <?php
