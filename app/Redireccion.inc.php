40b793f1b8c24c4d4bac683bbf5d1bb28847e3e7
Walter Díaz <lwalter_19@hotmail.com>
2020-04-05 10:58:30 -0600

    Creamos la clase Redireccion en app/ y creamos el metodo redirigir() el cual redirigira a la pagina de bienvenida

---
+++ app/Redireccion.inc.php
@@ -0,0 +1,9 @@
+<?php
+    class Redireccion{
+        
+        //Creamos la funcion para hacer la redireccion a nuestra pagína de bienvenida
+        public static function redirigir($url){
+            header("Location :" . $url, true, 301);
+            die();
+        }
+    }
