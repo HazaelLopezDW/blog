48be549e8e29e68cd32fa96a421d3e47a116ecdf
Hazael Lopez <elhazael134@gmail.com>
2020-04-09 08:21:01 -0600

    Creamos la base de datos, la usamos y creamos la tabla usuarios

--- blog.sql
+++ blog.sql
@@ -1,1 +1,13 @@
+CREATE DATABASE blog;
 
+USE blog;
+
+CREATE TABLE usuarios(
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    fecha_registro DATETIME NOT NULL,
+    activo TINYINT NOT NULL,
+    PRIMARY KEY(id)
+);
