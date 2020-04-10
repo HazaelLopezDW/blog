1baa700e2003cd387102174c757e5189fd81bdd0
Hazael Lopez <elhazael134@gmail.com>
2020-04-10 09:26:57 -0600

    Agregamos las variable a usar en la clase Entrada.inc.php y construimos nuestro constructor y lo metodos get and set

--- app/Entrada.inc.php
+++ app/Entrada.inc.php
@@ -1,1 +1,68 @@
+<?php
+    class Entrada{
+        
+        private $id;
+        private $autor_id;
+        private $titulo;
+        private $texto;
+        private $fecha;
+        private $activa;
+        
+        public function __construct($id, $autor_id, $titulo, $texto, $fecha, $activa) {
+            $this -> id = $id;
+            $this -> autor_id = $autor_id;
+            $this -> titulo = $titulo;
+            $this -> texto = $texto;
+            $this -> fecha = $fecha;
+            $this -> activa = $activa;
+        }
+        
+        public function obtener_id() {
+            return $this -> id;
+        }
 
+        public function obtener_autor_id() {
+            return $this -> autor_id;
+        }
+
+        public function obtener_titulo() {
+            return $this -> titulo;
+        }
+
+        public function obtener_texto() {
+            return $this -> texto;
+        }
+
+        public function obtener_fecha() {
+            return $this -> fecha;
+        }
+
+        public function obtener_activa() {
+            return $this -> activa;
+        }
+
+        public function cambiar_id($id) {
+            $this -> id = $id;
+        }
+
+        public function cambiar_autor_id($autor_id) {
+            $this -> autor_id = $autor_id;
+        }
+
+        public function cambiar_titulo($titulo) {
+            $this -> titulo = $titulo;
+        }
+
+        public function cambiar_texto($texto) {
+            $this -> texto = $texto;
+        }
+
+        public function cambiar_fecha($fecha) {
+            $this -> fecha = $fecha;
+        }
+
+        public function cambiar_activa($activa) {
+            $this -> activa = $activa;
+        }
+
+    }
