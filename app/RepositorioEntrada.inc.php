8e77021747b0193c147be5d173706d837affc91f
Hazael Lopez <elhazael134@gmail.com>
2020-04-10 19:45:25 -0600

    Modificamos la funccion insertar_entrada(,)

--- app/RepositorioEntrada.inc.php
+++ app/RepositorioEntrada.inc.php
@@ -8,14 +8,18 @@
         public static function insertar_entrada($conexion, $entrada){
             $insertar_entrada = null;
             
+            $autor_id = $entrada -> obtener_autor_id();
+            $titulo = $entrada -> obtener_titulo();
+            $texto = $entrada -> obtener_password();
+            
             if(isset($conexion)){
                 try{
                     
-                    $sql = "INSERT INTO usuarios(nombre, email, password, fecha_registro, activo) VALUES(:nombre, :email, :password, NOW(), 0)";
+                    $sql = "INSERT INTO entradas(autor_id, titulo, texto, fecha, activa) VALUES(:autor_id, :titulo, :texto, NOW(), 0)";
                     $sentencia = $conexion -> prepare($sql);
-                    $sentencia -> bindParam(":nombre", $usrNom, PDO::PARAM_STR);
-                    $sentencia -> bindParam(":email", $usrEmail, PDO::PARAM_STR);
-                    $sentencia -> bindParam(":password", $usrpassword, PDO::PARAM_STR);
+                    $sentencia -> bindParam(":autor_id", $autor_id, PDO::PARAM_STR);
+                    $sentencia -> bindParam(":titulo", $titulo, PDO::PARAM_STR);
+                    $sentencia -> bindParam(":texto", $texto, PDO::PARAM_STR);
                     
                     $insertar_entrada = $sentencia -> execute();
                 } catch (PDOException $ex) {
