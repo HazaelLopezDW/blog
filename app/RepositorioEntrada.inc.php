65fa5feef9d0152e431a2fc39921cddf535132c7
Hazael Lopez <elhazael134@gmail.com>
2020-04-10 19:43:33 -0600

    Creamos la funccion insertar_entrada(,) que nos servira para insertar las entradas masivamente

--- app/RepositorioEntrada.inc.php
+++ app/RepositorioEntrada.inc.php
@@ -1,1 +1,27 @@
-
+<?php
+    include_once 'app/Config.inc.php';
+    include_once 'app/Conexion.inc.php';
+    include_once 'app/Entrada.inc.php';
+    
+    class RepositorioEntrada{
+        
+        public static function insertar_entrada($conexion, $entrada){
+            $insertar_entrada = null;
+            
+            if(isset($conexion)){
+                try{
+                    
+                    $sql = "INSERT INTO usuarios(nombre, email, password, fecha_registro, activo) VALUES(:nombre, :email, :password, NOW(), 0)";
+                    $sentencia = $conexion -> prepare($sql);
+                    $sentencia -> bindParam(":nombre", $usrNom, PDO::PARAM_STR);
+                    $sentencia -> bindParam(":email", $usrEmail, PDO::PARAM_STR);
+                    $sentencia -> bindParam(":password", $usrpassword, PDO::PARAM_STR);
+                    
+                    $insertar_entrada = $sentencia -> execute();
+                } catch (PDOException $ex) {
+                    print "ERROR:" . $ex -> getMessage() . "<br>";
+                }
+            }
+            return $insertar_entrada;
+        }
+    }
