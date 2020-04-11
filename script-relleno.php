c57b1f083f570e4c8497effe3f17ec820e099d15
Hazael Lopez <elhazael134@gmail.com>
2020-04-10 20:03:45 -0600

    Incluimos los archivo y clases que nesecitamos y abrimos la conexion

--- script-relleno.php
+++ script-relleno.php
@@ -1,1 +1,14 @@
-
+<?php
+    include_once 'app/Config.inc.php';
+    include_once 'app/Conexion.inc.php';
+    
+    
+    include_once 'app/Usuario.inc.php';
+    include_once 'app/Entrada.inc.php';
+    include_once 'app/Comentariio.inc.php';
+    
+    include_once 'app/RepositorioUsuario.inc.php';
+    include_once 'app/RepositorioEntrada.inc.php';
+    include_once 'app/RepositorioComentario.inc.php';
+    
+    Conexion::abrir_conexion();
