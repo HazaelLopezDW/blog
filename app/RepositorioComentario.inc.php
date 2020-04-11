95b36052441df2d60588619e86070ae954ffa471
Hazael Lopez <elhazael134@gmail.com>
2020-04-10 19:57:00 -0600

    Creamos nuestra funcion insertar_comentario(,) para insertar los comentario en nuestro sitio web

--- app/RepositorioComentario.inc.php
+++ app/RepositorioComentario.inc.php
@@ -1,1 +1,33 @@
-
+<?php
+    include_once 'app/Config.inc.php';
+    include_once 'app/Conexion.inc.php';
+    include_once 'app/Comentario.inc.php';
+    
+    class RepositorioComentario{
+        
+        public static function insertar_comentario($conexion, $comentario){
+            $insertar_comentario = null;
+            
+            $autor_id = $comentario -> obtener_autor_id();
+            $entrada_id = $comentario -> obtener_entrada_id();
+            $titulo = $comentario -> obtener_titulo();
+            $texto = $comentario -> obtener_password();
+            
+            if(isset($conexion)){
+                try{
+                    
+                    $sql = "INSERT INTO comentario(autor_id, titulo, entrada_id, texto, fecha) VALUES(:autor_id, :entrada_id, :titulo, :texto, NOW())";
+                    $sentencia = $conexion -> prepare($sql);
+                    $sentencia -> bindParam(":autor_id", $autor_id, PDO::PARAM_STR);
+                    $sentencia -> bindParam(":entrada_id", $entrada_id, PDO::PARAM_STR);
+                    $sentencia -> bindParam(":titulo", $titulo, PDO::PARAM_STR);
+                    $sentencia -> bindParam(":texto", $texto, PDO::PARAM_STR);
+                    
+                    $insertar_comentario = $sentencia -> execute();
+                } catch (PDOException $ex) {
+                    print "ERROR:" . $ex -> getMessage() . "<br>";
+                }
+            }
+            return $insertar_comentario;
+        }
+    }
