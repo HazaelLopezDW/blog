e25dd93c0a39fca5758badce5827a449d789f501
Hazael Lopez <elhazael134@gmail.com>
2020-04-16 10:20:32 -0600

    Borramos el .php en el archivo Config.inc.php para que las rutas funcionen corectamente

--- app/Config.inc.php
+++ app/Config.inc.php
@@ -7,13 +7,13 @@
     
     // Rutas de el sitio Web
     define("SERVIDOR", "http://localhost/blog/");
-    define("RUTA_ENTRADAS", SERVIDOR."entradas.php");
-    define("RUTA_FAVORITOS", SERVIDOR."favoritos.php");
-    define("RUTA_AUTORES", SERVIDOR."autores.php");
-    define("RUTA_USUARIOS", SERVIDOR."usuarios.php");
-    define("RUTA_LOGIN", SERVIDOR."login.php");
-    define("RUTA_REGISTRO", SERVIDOR."registro.php");
-    define("RUTA_REGISTRO_CORRECTO", SERVIDOR."registro-correcto.php");
+    define("RUTA_ENTRADAS", SERVIDOR."entradas");
+    define("RUTA_FAVORITOS", SERVIDOR."favoritos");
+    define("RUTA_AUTORES", SERVIDOR."autores");
+    define("RUTA_USUARIOS", SERVIDOR."usuarios");
+    define("RUTA_LOGIN", SERVIDOR."login");
+    define("RUTA_REGISTRO", SERVIDOR."registro");
+    define("RUTA_REGISTRO_CORRECTO", SERVIDOR."registro-correcto");
     
     //Recursos
     define("RUTA_CSS", SERVIDOR."/css/");
