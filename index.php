8c448e0450dd46e71c21df7bc1efb111339113bf
Hazael Lopez <elhazael134@gmail.com>
2020-04-16 10:12:40 -0600

    Creamos nuestro enrutador en index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
+<?php
+    $componentes_url = parse_url($_SERVER['REQUEST_URI']);
+    
+    $ruta = $componentes_url['path'];
+    
+    $partes_ruta = explode("/", $ruta);
 
+    if($partes_ruta[2] === "registro"){
+        include_once 'vistas/registro.php';
+    }else if($partes_ruta[2] === "login"){
+        include_once 'vistas/login.php';
+    }else if($partes_ruta[1] === "blog"){
+        include_once 'vistas/home.php';
+    }else{
+        echo "404";
+    }
+    
+    
