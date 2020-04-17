164274ea2d90b82da33250de0106893fd331ed33
Walter Díaz <lwalter_19@hotmail.com>
2020-04-17 10:54:03 -0600

    Agregamos en comentario las rutas para nuestros colaboradores

--- index.php
+++ index.php
@@ -4,7 +4,15 @@
     $ruta = $componentes_url['path'];
     
     $partes_ruta = explode("/", $ruta);
-
+    
+    /*
+    echo "partes_ruta[0] -> " . $partes_ruta[0] . "<br>";
+    echo "partes_ruta[1] -> " . $partes_ruta[1] . "<br>";
+    echo "partes_ruta[2] -> " . $partes_ruta[2] . "<br>";
+    echo "partes_ruta[3] -> " . $partes_ruta[3] . "<br>";
+    echo "partes_ruta[4] -> " . $partes_ruta[4] . "<br>"; 
+    */
+    
     if($partes_ruta[2] === "registro"){
         include_once 'vistas/registro.php';
     }else if($partes_ruta[2] === "login"){
@@ -14,5 +22,21 @@
     }else{
         echo "404";
     }
+    
+    
+    
+    /*
+    if($partes_ruta[3] === "registro"){
+        include_once 'vistas/registro.php';
+    }else if($partes_ruta[3] === "login"){
+        include_once 'vistas/login.php';
+    }else if($partes_ruta[3] === "script-relleno"){
+        include_once  'herramientas/script-relleno.php';
+    }else if($partes_ruta[2] === "blog"){
+        include_once 'vistas/home.php';
+    }else{
+        echo "404";
+    }
+    */
     
     
