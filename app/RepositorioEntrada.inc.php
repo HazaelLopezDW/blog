782b9c293ce7d0d7507ed50117c934eabc3c8ffa
Hazael Lopez <elhazael134@gmail.com>
2020-04-18 11:53:56 -0600

    Agregamos fila['url'] en la funcion obtener_todas_por_fecha_desendiente()

--- app/RepositorioEntrada.inc.php
+++ app/RepositorioEntrada.inc.php
@@ -45,7 +45,8 @@
                     if(count($resultado)){
                         
                         foreach ($resultado as $fila){
-                            $entradas[] = new Entrada($fila['id'], $fila['autor_id'], $fila['titulo'], $fila['texto'], $fila['fecha'], $fila['activa']);
+                            $entradas[] = new Entrada($fila['id'], $fila['autor_id'], $fila['url'], $fila['titulo'], $fila['texto'], $fila['fecha'], 
+                                    $fila['activa']);
                         }
                     }
                 } catch (PDOException $ex) {
