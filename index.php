aa56240a6c72cb4a17ecd66cefcdc465c9606b0f
Hazael Lopez <elhazael134@gmail.com>
2020-04-18 12:35:10 -0600

    Hicimos la inclusion de los documento que vamos a necesitar para que index.php funcione correctamente

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
+    include_once 'app/Config.inc.php';
+    include_once 'app/Conexion.inc.php';
+    
+    include_once 'app/Usuario.inc.php';
+    include_once 'app/Entrada.inc.php';
+    include_once 'app/Comentario.inc.php';
+    
+    include_once 'app/RepositorioUsuario.inc.php';
+    include_once 'app/RepositorioEntrada.inc.php';
+    include_once 'app/RepositorioComentario.inc.php';
+
     $componentes_url = parse_url($_SERVER['REQUEST_URI']);
     
     $ruta = $componentes_url['path'];
