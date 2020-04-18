4314aaa6582fdd6461e2a05b37dd696e08ac306a
Hazael Lopez <elhazael134@gmail.com>
2020-04-18 12:36:16 -0600

    incluimos los los archivos y mostramos el titulo y texto de la entrada

--- vistas/entrada.php
+++ vistas/entrada.php
@@ -1,1 +1,15 @@
-
+<?php
+    include_once 'app/Config.inc.php';
+    include_once 'app/Conexion.inc.php';
+    
+    include_once 'app/Usuario.inc.php';
+    include_once 'app/Entrada.inc.php';
+    include_once 'app/Comentario.inc.php';
+    
+    include_once 'app/RepositorioUsuario.inc.php';
+    include_once 'app/RepositorioEntrada.inc.php';
+    include_once 'app/RepositorioComentario.inc.php';
+    
+    echo $entrada -> obtener_titulo();
+    echo "<br>";
+    echo $entrada -> obtener_texto();
