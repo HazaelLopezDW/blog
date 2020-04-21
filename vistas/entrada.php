c4f2d645416e8664e954d978c3ea72eed39511db
Hazael Lopez <elhazael134@gmail.com>
2020-04-21 16:07:31 -0600

    Creamos las etiquetas con las clases necesarias para que el articulo se muestre de la mejor manera

--- vistas/entrada.php
+++ vistas/entrada.php
@@ -10,6 +10,49 @@
     include_once 'app/RepositorioEntrada.inc.php';
     include_once 'app/RepositorioComentario.inc.php';
     
-    echo $entrada -> obtener_titulo();
-    echo "<br>";
-    echo $entrada -> obtener_texto();
+    $titulo = $entrada -> obtener_titulo();
+    
+    include_once 'plantillas/documento-inicio.inc.php';
+    include_once 'plantillas/navbar.inc.php';    
+?>
+
+<div class="container contenido-articulo">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>
+                <?php
+                    echo $entrada -> obtener_titulo();
+                ?>
+            </h1>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-md-12">
+            <p>
+                Por:
+                <a href="#">
+                    <span class="glyphicon glyphicon-user" data-hidden="true"></span><?php echo $autor -> obtener_nombre(); ?>
+                </a>
+                El: 
+                <?php
+                    echo $entrada -> Obtener_fecha();
+                ?>
+            </p>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-md-12">
+            <article class="text-justify">
+                <?php
+                    echo nl2br($entrada -> obtener_texto());
+                ?>
+            </article>
+        </div>
+    </div>
+</div>
+
+<?php
+    include_once 'plantillas/documento-cierre.inc.php';
+?>
