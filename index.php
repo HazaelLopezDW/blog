7695ca0135edc627812fc00b96e75cde8d80a0ee
Hazael Lopez <elhazael134@gmail.com>
2020-04-21 16:06:16 -0600

    Cremos la variable _autor que obtiene un usuarios por el id

--- index.php
+++ index.php
@@ -60,6 +60,8 @@
                 $entrada = RepositorioEntrada::obtener_entrada_por_url(Conexion::obtener_conexion(), $url);
                 
                 if($entrada != null){
+                    $autor = RepositorioUsuario::obtener_usuario_por_id(Conexion::obtener_conexion(), $entrada -> obtener_autor_id());
+                    
                     $ruta_elegida = "vistas/entrada.php";
                 }
             }
