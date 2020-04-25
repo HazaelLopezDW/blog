c4338d0b1358b0a855ff2c4a0d48f7eb604292e8
Hazael Lopez <elhazael134@gmail.com>
2020-04-25 17:24:28 -0600

    Creamos un contenerdor con dos filas y una menu

--- plantillas/panel_control_declaracion.inc.php
+++ plantillas/panel_control_declaracion.inc.php
@@ -1,1 +1,10 @@
-
+<div class="container">
+    <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+            <ul class="nav nav-sidebar">
+                <li><a href="#">Entradas</a></li>
+                <li><a href="#">Comentarios</a></li>
+                <li><a href="#">Favoritos</a></li>
+            </ul>
+        </div>
+        <div class="col-sm-9 col-md-10 main">
