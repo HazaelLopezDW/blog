580e50a3edfe90e2d70274f66fc57728cd505a50
Hazael Lopez <elhazael134@gmail.com>
2020-04-25 18:06:11 -0600

    incluimos un switch para ver en que gestor estamos

--- vistas/gestor.php
+++ vistas/gestor.php
@@ -1,1 +1,23 @@
-
+<?php
+    include_once 'plantillas/documento-inicio.inc.php';
+    include_once 'plantillas/navbar.inc.php';
+    include_once 'plantillas/panel_control_declaracion.inc.php';
+    
+    // activariamos gestro actual
+    switch ($gestor_actual){
+        case "":
+            include_once 'plantillas/gestor-generico.inc.php';
+            break;
+        case "entradas":
+            include_once 'plantillas/gestor-entradas.inc.php';
+            break;
+        case "comentarios":
+            include_once 'plantillas/gestor-comentarios.inc.php';
+            break;
+        case "favoritos":
+            include_once 'plantillas/gestor-favoritos.inc.php';
+            break;
+    }
+    
+    include_once 'plantillas/panel_control_cierre.inc.php';
+?>
