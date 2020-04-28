9cbeae4303d2d3be220ff19b48667c7d521a482e
Hazael Lopez <elhazael134@gmail.com>
2020-04-28 17:21:29 -0600

    Creamos la variable _cantidad_entradas_inactivas y mandamos a llamar al metodo contar_entradas_inactivas_usuario(_conexion, _id_usuario)

--- vistas/gestor.php
+++ vistas/gestor.php
@@ -7,6 +7,7 @@
     switch ($gestor_actual){
         case "":
             $cantidad_entradas_activas = RepositorioEntrada::contar_entradas_activas_usuario(Conexion::obtener_conexion(), $_SESSION['id_usuario']);
+            $cantidad_entradas_inactivas = RepositorioEntrada::contar_entradas_inactivas_usuario(Conexion::obtener_conexion(), $_SESSION['id_usuario']);
             
             include_once 'plantillas/gestor-generico.inc.php';
             break;
