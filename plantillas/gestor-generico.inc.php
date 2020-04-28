3fdf4eae1b85854f7ab36bc2b73ebd33b6d85a51
Hazael Lopez <elhazael134@gmail.com>
2020-04-28 11:30:50 -0600

    Maquetamos nuestro gestor generico

--- plantillas/gestor-generico.inc.php
+++ plantillas/gestor-generico.inc.php
@@ -1,1 +1,67 @@
-GENERICO
+<div class="row text-center">
+    <div class="col-md-4 gg-elemento" id="gg-entradas">
+        <h2>
+            <svg class="bi bi-chat-dots" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M2.678 11.894a1 1 0 01.287.801 10.97 10.97 0 01-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 01.71-.074A8.06 8.06 0 008 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 01-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 00.244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 01-2.347-.306c-.52.263-1.639.742-3.468 1.105z" clip-rule="evenodd"/>
+                <path d="M5 8a1 1 0 11-2 0 1 1 0 012 0zm4 0a1 1 0 11-2 0 1 1 0 012 0zm4 0a1 1 0 11-2 0 1 1 0 012 0z"/>
+            </svg>
+        </h2>
+        <h3>Entradas</h3>
+        <hr>
+            <h4>129</h4>
+            <h5>Entradas publicadas</h5>
+            <br>
+            <h4>24</h4>
+            <h5>Borradores</h5>
+            <br>
+            <h4>1024</h4>
+            <h5>Likes</h5>
+            <br>
+            <h4>45</h4>
+            <h5>DisLikes</h5>
+            <br>
+    </div>
+    <div class="col-md-4 gg-elemento" id="gg-comentarios">
+        <h2>
+            <svg class="bi bi-newspaper" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M0 2A1.5 1.5 0 011.5.5h11A1.5 1.5 0 0114 2v12a1.5 1.5 0 01-1.5 1.5h-11A1.5 1.5 0 010 14V2zm1.5-.5A.5.5 0 001 2v12a.5.5 0 00.5.5h11a.5.5 0 00.5-.5V2a.5.5 0 00-.5-.5h-11z" clip-rule="evenodd"/>
+                <path fill-rule="evenodd" d="M15.5 3a.5.5 0 01.5.5V14a1.5 1.5 0 01-1.5 1.5h-3v-1h3a.5.5 0 00.5-.5V3.5a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
+                <path d="M2 3h10v2H2V3zm0 3h4v3H2V6zm0 4h4v1H2v-1zm0 2h4v1H2v-1zm5-6h2v1H7V6zm3 0h2v1h-2V6zM7 8h2v1H7V8zm3 0h2v1h-2V8zm-3 2h2v1H7v-1zm3 0h2v1h-2v-1zm-3 2h2v1H7v-1zm3 0h2v1h-2v-1z"/>
+            </svg>
+        </h2>
+        <h3>Comentarios</h3>
+        <hr>
+            <h4>129</h4>
+            <h5>Entradas publicadas</h5>
+            <br>
+            <h4>24</h4>
+            <h5>Borradores</h5>
+            <br>
+            <h4>1024</h4>
+            <h5>Likes</h5>
+            <br>
+            <h4>45</h4>
+            <h5>DisLikes</h5>
+            <br>
+    </div>
+    <div class="col-md-4 gg-elemento" id="gg-favoritos">
+        <h2>
+            <svg class="bi bi-star" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 00-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 00-.163-.505L1.71 6.745l4.052-.576a.525.525 0 00.393-.288l1.847-3.658 1.846 3.658a.525.525 0 00.393.288l4.052.575-2.906 2.77a.564.564 0 00-.163.506l.694 3.957-3.686-1.894a.503.503 0 00-.461 0z" clip-rule="evenodd"/>
+            </svg>
+        </h2>
+        <h3>Favoritos</h3>
+        <hr><h4>129</h4>
+            <h5>Entradas publicadas</h5>
+            <br>
+            <h4>24</h4>
+            <h5>Borradores</h5>
+            <br>
+            <h4>1024</h4>
+            <h5>Likes</h5>
+            <br>
+            <h4>45</h4>
+            <h5>DisLikes</h5>
+            <br>
+    </div>
+</div>
