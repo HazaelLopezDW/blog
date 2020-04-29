8fb46b8d7e438d5d03c0a78856a0040def7d8090
Hazael Lopez <elhazael134@gmail.com>
2020-04-29 10:26:21 -0600

    Maquetamos nuestro gestor de entrada y creamos una tabla con estilos de bootstrap

--- plantillas/gestor-entradas.inc.php
+++ plantillas/gestor-entradas.inc.php
@@ -1,1 +1,44 @@
-GESTOR ENTRADAS
+<div class="row parte-gestor-entradas">
+    <div class="col-md-12">
+        <h1>Gestión de entradas</h1>
+        <br><br>
+    </div>
+</div>
+<div class="row parte-gestor-entradas">
+    <div class="col-ms-12">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Fecha</th>
+                    <th>Título</th>
+                    <th>Estado</th>
+                    <th>Comentarios</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>bla bla</td>
+                    <td>bla bla</td>
+                    <td>bla bla</td>
+                    <td>bla bla</td>
+                    <td>bla bla</td>
+                </tr>
+                <tr>
+                    <td>bla bla</td>
+                    <td>bla bla</td>
+                    <td>bla bla</td>
+                    <td>bla bla</td>
+                    <td>bla bla</td>
+                </tr>
+                <tr>
+                    <td>bla bla</td>
+                    <td>bla bla</td>
+                    <td>bla bla</td>
+                    <td>bla bla</td>
+                    <td>bla bla</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
