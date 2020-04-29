4941d3060c593d1b8546140400d265432739ac22
Hazael Lopez <elhazael134@gmail.com>
2020-04-29 15:01:33 -0600

    Insertamos contenido en la tabla con php

--- plantillas/gestor-entradas.inc.php
+++ plantillas/gestor-entradas.inc.php
@@ -6,39 +6,44 @@
 </div>
 <div class="row parte-gestor-entradas">
     <div class="col-ms-12">
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>Fecha</th>
-                    <th>Título</th>
-                    <th>Estado</th>
-                    <th>Comentarios</th>
-                    <th>Acciones</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>bla bla</td>
-                    <td>bla bla</td>
-                    <td>bla bla</td>
-                    <td>bla bla</td>
-                    <td>bla bla</td>
-                </tr>
-                <tr>
-                    <td>bla bla</td>
-                    <td>bla bla</td>
-                    <td>bla bla</td>
-                    <td>bla bla</td>
-                    <td>bla bla</td>
-                </tr>
-                <tr>
-                    <td>bla bla</td>
-                    <td>bla bla</td>
-                    <td>bla bla</td>
-                    <td>bla bla</td>
-                    <td>bla bla</td>
-                </tr>
-            </tbody>
-        </table>
+        <?php
+        if (count($array_entradas) > 0) {
+            ?>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Fecha</th>
+                        <th>Título</th>
+                        <th>Estado</th>
+                        <th>Comentarios</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        for($i = 0; $i < count($array_entradas); $i++){
+                            $entrada_actual = $array_entradas[$i][0];
+                            $comentarios_entrada_actual = $array_entradas[$i][1];
+                            ?>
+                    <tr>
+                        <td><?php echo $entrada_actual -> obtener_fecha(); ?></td>
+                        <td><?php echo $entrada_actual -> obtener_titulo(); ?></td>
+                        <td><?php echo $entrada_actual -> obtener_activa(); ?></td>
+                        <td><?php echo $comentarios_entrada_actual; ?></td>
+                        <td>bla bla</td>
+                    </tr>
+                    <?php
+                        }
+                    ?>
+                </tbody>
+            </table>
+            <?php
+        } else {
+            ?>
+            <h3 class="text-center">Todavia no has escrito ninguna entrada</h3>
+            <br><br>
+            <?php
+        }
+        ?>
     </div>
 </div>
