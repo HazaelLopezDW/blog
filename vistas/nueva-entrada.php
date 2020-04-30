e629660fe7573932e45771c571abe60e7bc86053
Hazael Lopez <elhazael134@gmail.com>
2020-04-30 15:41:14 -0600

    Pusimos los elemento necesarios para el buen funcionamiento del formulario

--- vistas/nueva-entrada.php
+++ vistas/nueva-entrada.php
@@ -19,20 +19,26 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <form class="form-nueva-entrada" role="form"> 
+            <form class="form-nueva-entrada" role="form" method="post" action="<?php echo RUTA_NUEVA_ENTRADA; ?>"> 
                 <div class="form-group">
                     <label for="titulo">Titulo</label>
-                    <input id="titulo" type="text" class="form-control">
+                    <input id="titulo" type="text" class="form-control" name="titulo" placeholder="Ponle un titulo a esta entrada">
+                </div>
+                <div class="form-group">
+                    <label for="url">URL</label>
+                    <input id="url" type="text" class="form-control" name="url" placeholder="Dereccion unica sin espacios para la entrada">
                 </div>
                 <div class="form-group">
                     <label for="contenido">Contenido</label>
-                    <textarea id="contenido" class="form-control" rows="5"></textarea>
+                    <textarea id="contenido" class="form-control" rows="5" name="texto" placeholder="Escribe aquí tu articulo"></textarea>
                 </div>
                 <div class="checkbox">
-                    <label><input type="checkbox" value="publica">Marca este recuadro si quieres que la entrada se publique de inmediato</label>
+                    <label>
+                        <input type="checkbox" name="publicar" value="si">Marca este recuadro si quieres que la entrada se publique de inmediato
+                    </label>
                 </div>
                 <br>
-                <button type="submit" class="btn btn-default btn-primary">Guaradar Entrada</button>
+                <button type="submit" class="btn btn-default btn-primary" name="guardar">Guaradar Entrada</button>
             </form>
         </div>
     </div>
