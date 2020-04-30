4258ce29e071a1c04dfcdbbb2a9166fbfb51a65c
Hazael Lopez <elhazael134@gmail.com>
2020-04-30 10:24:52 -0600

    Agregamos una nueva ruta y modificamos las rutas anteriores

--- app/Config.inc.php
+++ app/Config.inc.php
@@ -6,20 +6,21 @@
     define("NOMBRE_BD", "blog");
     
     // Rutas de el sitio Web
-    define("SERVIDOR", "http://localhost/blog/");
-    define("RUTA_ENTRADAS", SERVIDOR."entradas");
-    define("RUTA_FAVORITOS", SERVIDOR."favoritos");
-    define("RUTA_AUTORES", SERVIDOR."autores");
-    define("RUTA_USUARIOS", SERVIDOR."usuarios");
-    define("RUTA_LOGIN", SERVIDOR."login");
-    define("RUTA_LOGOUT", SERVIDOR."logout");
-    define("RUTA_REGISTRO", SERVIDOR."registro");
-    define("RUTA_REGISTRO_CORRECTO", SERVIDOR."registro-correcto");
-    define("RUTA_ENTRADA", SERVIDOR."entrada");
-    define("RUTA_GESTOR", SERVIDOR."gestor");
+    define("SERVIDOR", "http://localhost/blog");
+    define("RUTA_ENTRADAS", SERVIDOR."/entradas");
+    define("RUTA_FAVORITOS", SERVIDOR."/favoritos");
+    define("RUTA_AUTORES", SERVIDOR."/autores");
+    define("RUTA_USUARIOS", SERVIDOR."/usuarios");
+    define("RUTA_LOGIN", SERVIDOR."/login");
+    define("RUTA_LOGOUT", SERVIDOR."/logout");
+    define("RUTA_REGISTRO", SERVIDOR."/registro");
+    define("RUTA_REGISTRO_CORRECTO", SERVIDOR."/registro-correcto");
+    define("RUTA_ENTRADA", SERVIDOR."/entrada");
+    define("RUTA_GESTOR", SERVIDOR."/gestor");
     define("RUTA_GESTOR_ENTRADAS", RUTA_GESTOR."/entradas");
     define("RUTA_GESTOR_COMENTARIOS", RUTA_GESTOR."/comentarios");
     define("RUTA_GESTOR_FAVORITOS", RUTA_GESTOR."/favoritos");
+    define("RUTA_NUEVA_ENTRADA", SERVIDOR."/favoritos");
     
     //Recursos
     define("RUTA_CSS", SERVIDOR."css/");
