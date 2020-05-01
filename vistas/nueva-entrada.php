8826f4c3ab704645f255d293cc75b15ca5e85589
Hazael Lopez <elhazael134@gmail.com>
2020-05-01 09:33:27 -0600

    Incluimos algunos archivos que utilizaremos en nueva-entrada y verificamos si el formulario ha sido enviado creamos nuestros objetos de ValidadorEntrada he insertamos la entrada en la DB

--- vistas/nueva-entrada.php
+++ vistas/nueva-entrada.php
@@ -1,8 +1,38 @@
 <?php
     include_once 'app/Config.inc.php';
     include_once 'app/Conexion.inc.php';
-    include_once 'app/RepositorioUsuario.inc.php';
-    include_once 'app/ValidadorRegistro.inc.php';
+    include_once 'app/Entrada.inc.php';
+    include_once 'app/RepositorioEntrada.inc.php';
+    include_once 'app/ValidadorEntrada.inc.php';
+    include_once 'app/ControlSesion.inc.php';
+    include_once 'app/Redireccion.inc.php';
+    
+    $entrada_publica = 0;
+    if(isset($_POST['guardar'])){
+        Conexion::abrir_conexion();
+        
+        $validador = new ValidadorEntrada($_POST['titulo'], $_POST['url'], htmlspecialchars($_POST['texto']), Conexion::obtener_conexion());
+        
+        if(isset($_POST['publicar']) && $_POST['publicar'] == "si"){
+            $entrada_publica = 1;
+        }
+        if($validador -> entrada_valida()){
+            if(ControlSesion :: sesion_iniciada()){
+                $entrada = new Entrada("", $_SESSION['id_usuario'], $validador -> obtener_url(), $validador -> obtener_titulo(), 
+                        $validador -> obtener_texto(), "", $entrada_publica);
+                
+                $entrada_insertada = RepositorioEntrada::insertar_entrada(Conexion::obtener_conexion(), $entrada);
+                if($entrada_insertada){
+                    Redireccion::redirigir(RUTA_GESTROR_ENTRADAS);
+                }
+            }else{
+                Redireccion::redirigir(RUTA_LOGIN);
+            }
+            Conexion::cerrar_conexion();
+        }
+    }
+    
+    include_once '';
     
     $titulo = "Nueva Entrada del blog";
     
