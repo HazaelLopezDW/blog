2e1c1a33c81036c702beb244f1b9ba288dfabaf3
Hazael Lopez <elhazael134@gmail.com>
2020-05-01 09:12:12 -0600

    Terminamos de crear el ValidadorEntrada.inc.php

--- app/ValidadorEntrada.inc.php
+++ app/ValidadorEntrada.inc.php
@@ -51,4 +51,97 @@ include_once 'RepositorioEntrada.inc.php';
                 return "Ya existe un entrada con ese titulo, por favor escoge uno diferente.";
             }
         }
+        
+        private function validar_url($conexion, $url){
+            if(!$this -> variable_iniciada($url)){
+                return "Debes insertar una url";
+            }else{
+                $this -> url = $url;
+            }
+            
+            if(strlen($url) != strlen(trim($url))){
+                return "La URL no puede tener espacios";
+            }
+            
+            if(RepositorioEntrada::url_existe($conexion, $url)){
+                return "Ya existe otro articulo con la misma URL eligue otra diferente";
+            }
+        }
+        
+        private function validar_texto($conexion, $texto){
+            if(!$this -> variable_iniciada($texto)){
+                return "El contenido no puede estar vacio";
+            }else{
+                $this -> texto = $texto;
+            }
+        }
+        
+        
+        public function obtener_titulo(){
+            return $this -> titulo;
+        }
+        
+        public function obtener_url(){
+            return $this -> url;
+        }
+        
+        public function obtener_texto(){
+            return $this -> texto;
+        }
+        
+        
+        public function mostrar_titulo(){
+            if($this -> titulo != ""){
+                echo "value='" . $this -> titulo . "'";
+            }
+        }
+        
+        public function mostrar_url(){
+            if($this -> url != ""){
+                echo "value='" . $this -> url . "'";
+            }
+        }
+        
+        public function mostrar_texto(){
+            if($this -> texto != "" && strlen(trim($this -> texto)) > 0){
+                echo $this -> texto;
+            }
+        }
+        
+        
+        public function mostrar_error_titulo(){
+            if(!$this -> error_titulo != ""){
+                echo $this -> aviso_inicio . $this -> error_titulo . $this -> aviso_cierre;
+            }
+        }
+        
+        public function mostrar_error_url(){
+            if(!$this -> error_url != ""){
+                echo $this -> aviso_inicio . $this -> error_url . $this -> aviso_cierre;
+            }
+        }
+        
+        public function mostrar_error_texto(){
+            if(!$this -> error_texto != ""){
+                echo $this -> aviso_inicio . $this -> error_texto . $this -> aviso_cierre;
+            }
+        }
+        
+        public function entrada_valida(){
+            if( $this -> error_titulo == "" && $this -> error_url == "" && $this -> error_texto == ""){
+                return true;
+            }else{
+                return false;
+            }
+        }
     }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
