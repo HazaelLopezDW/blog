1400ec5a9b7cde2c17187bcbcbeb45c40924d5bb
Hazael Lopez <elhazael134@gmail.com>
2020-05-02 09:18:04 -0600

    Por el momento solo pegamos lo que tiene el formulario su botones he inputs

--- plantillas/form_nueva_entrada_validado.inc.php
+++ plantillas/form_nueva_entrada_validado.inc.php
@@ -1,1 +1,19 @@
-
+<div class="form-group">
+    <label for="titulo">Titulo</label>
+    <input id="titulo" type="text" class="form-control" name="titulo" placeholder="Ponle un titulo a esta entrada">
+</div>
+<div class="form-group">
+    <label for="url">URL</label>
+    <input id="url" type="text" class="form-control" name="url" placeholder="Dereccion unica sin espacios para la entrada">
+</div>
+<div class="form-group">
+    <label for="contenido">Contenido</label>
+    <textarea id="contenido" class="form-control" rows="5" name="texto" placeholder="Escribe aquí tu articulo"></textarea>
+</div>
+<div class="checkbox">
+    <label>
+        <input type="checkbox" name="publicar" value="si">Marca este recuadro si quieres que la entrada se publique de inmediato
+    </label>
+</div>
+<br>
+<button type="submit" class="btn btn-default btn-primary" name="guardar">Guaradar Entrada</button>
