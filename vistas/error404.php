083fa4735505ec9a5bb52f21af86c79b727f17fd
Hazael Lopez <elhazael134@gmail.com>
2020-07-21 10:24:16 -0600

    Creamos nuestra vista vista/error404.php

--- vistas/error404.php
+++ vistas/error404.php
@@ -1,3 +1,18 @@
 <?php
     header($_SERVER['SERVER_PROTOCOL'] . '404 Not Found', true, 404);
-    echo "La pagina no existe";
+    
+    $titulo = "Not Found";
+    
+    include_once  'plantillas/documento-inicio.inc.php';
+   ?>
+
+<div class="erro404  container-fluid text-center">
+    <div class="jumbotron">
+        <h1 class="text-center">Error 404 Not Found</h1>
+        <img src="https://firebasestorage.googleapis.com/v0/b/reactjs-1cb1d.appspot.com/o/imgReactJS%2F404.png?alt=media&token=80578f13-8875-4093-a515-aa102f657d1a"/>
+    </div>
+</div>
+
+<?php
+   include_once 'plantillas/documento-cierre.inc.php';
+?>
