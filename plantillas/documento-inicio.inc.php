a656cd3c7ef7013f1def63e16877fe05bafada61
Hazael Lopez <elhazael134@gmail.com>
2020-07-22 00:04:45 -0600

    No funcionas muchas cosas si nos pasamos a la ultima version de bootstrap

--- plantillas/documento-inicio.inc.php
+++ plantillas/documento-inicio.inc.php
@@ -12,7 +12,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="MetaData para los motores de busqueda">
         
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="<?php echo RUTA_CSS; ?>bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="<?php echo RUTA_CSS; ?>estilos.css">
     </head>
     <body>
